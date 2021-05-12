9f2b5987e71ac8edc3555ad0263cf80e234058ec
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-05-12 18:59:48 -0300

    first commit

---
+++ Marvel Comics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvel Comics
+//
+//  Created by Henrique Silva on 12/05/21.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
