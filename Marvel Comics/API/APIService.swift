0511f8b6fc69ad1bd007986284045aa6783557ff
edyama <ey.yamagata@gmail.com>
2021-05-20 13:11:29 -0300

    APIService adicionado e ViewController atualizado

--- Marvel Comics/API/APIService.swift
+++ Marvel Comics/API/APIService.swift
@@ -9,17 +9,29 @@ import UIKit
 import Alamofire
 import CryptoKit
 
-class Credentials {
+class ApiService {
+    public static let get = HTTPMethod(rawValue: "GET")
+    let baseURL: String = "http://gateway.marvel.com/v1/public/characters"
+    let privateKey = "beb6b6c6e2b6c26563514092219d56e717655602"
+    let publicKey = "53d86db4b4f54265554d105320c3d7e7"
+    let ts = String(Date().timeIntervalSince1970)
     
-    func searchCharacters() {
-        let ts = String(Date().timeIntervalSince1970)
-        let hash = md5(data: "\(ts)\(privateKey)\(publicKey)")
-        let url = "https://gateway.marvel.com:443/v1/public/characters?ts=\(ts)&apiKey=\(publicKey)&hash=\(hash)"
-        let parameters = ["ts": ts, "hash" : hash,  "publicKey" : publicKey]
-        let request = AF.request(url, parameters: parameters)
+    public func apiRequest() {
+        
+        let parameters = ["ts": ts, "hash" : self.getMD5(),  "publicKey" : publicKey]
+        
+        let request = AF.request(baseURL, parameters: parameters)
         // 2
         request.responseJSON { (data) in
             print(data)
+            
         }
+    }
+    
+    private func getMD5() -> String {
+        let apiData = ts + privateKey + publicKey
+        guard let data = apiData.data(using: .utf8) else { return String() }
+        let apiHash = Insecure.MD5.hash(data: data)
+        return apiHash.map { String(format: "%02hhx", $0) }.joined()
     }
 }
